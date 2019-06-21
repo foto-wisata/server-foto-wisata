1aa0dc6e593f8a7d01ea314620f1b0d37a5a15a7
wildannugroho <wildanagung34@gmail.com>
2019-06-21 11:17:09 +0700

    tambahan sedikit

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -1,6 +1,7 @@
 const User = require('../models/userModel')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const {comparePassword, generateJWT} = require('../Helpers/crypt')
 
 class UserController{
     static create(req, res, next){
@@ -26,29 +27,58 @@ class UserController{
     }
     static login(req, res, next){
         console.log('pasti masuk')
-        User.findOne({username : req.body.username})
+        User.findOne({email : req.body.email})
         .then(found => {
             console.log('ketemu 1')
             if(found){
-                bcrypt.compare(req.body.password, found.password, function(err, sucess) {
-                    if(sucess){
-                        console.log('bro')
-                        let payload = {
-                            username : found.username,
-                            email : found.email,
-                            id : found.id
-                        }
-                        let token = jwt.sign(payload, process.env.JWT_SECRET)
-                        res.json({token})
-                    }else{
-                        console.log('eo')
-                        throw ({status : 401, msg : 'wrong inputs'})
+                let success = comparePassword(req.body.password, found.password)
+                if(success){
+                    let payload = {
+                        username : found.username,
+                        email : found.email,
+                        id : found.id
                     }
-                })
+                    let token = generateJWT(payload)
+                    res.status(200).json({token, payload})
+                }
             }else{
                 console.log('else luar')
                 throw {msg : 'salah uiy'}
             }
+        })
+        .catch(next)
+    }
+    static googleSignIn(req, res, next){
+        console.log('masuk')
+        const client = new OAuth2Client(process.env.Google_id)
+        client.verifyIdToken({
+            idToken : req.body.idToken
+        })
+        .then(ticket => {
+            console.log(ticket, 'dapet ticket')
+            const payload = ticket.getPayload()
+            User.findOne({
+                email : payload.email
+            })
+            .then(function(found){
+                console.log(found, 'nemu found')
+                if(found){
+                    let token = jwt.sign({
+                        email : found.email,
+                        _id : found._id
+                    }, process.env.JWT_SECRET)
+                    res.json({
+                        token
+                    })
+                }else{
+                    console.log('bikin signup')
+                    req.body.email = payload.email
+                    req.body.password = 'qwertyu'
+                    console.log(req.body)
+                    UserController.signup(req, res, next)
+                }
+            })
+            console.log(payload)
         })
         .catch(next)
     }
